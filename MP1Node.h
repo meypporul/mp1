607da478864b0b07cf1f9b01a2a24f0debff683d
meypporul <56399482+meypporul@users.noreply.github.com>
2019-10-17 10:25:14 -0400

    Update MP1Node.h

--- MP1Node.h
+++ MP1Node.h
@@ -55,6 +55,7 @@ private:
 	Params *par;
 	Member *memberNode;
 	char NULLADDR[6];
+	void MP1Node::printMessage(string callr_fn, Address *sendr, MessageHdr *msg, int size);
 
 public:
 	MP1Node(Member *, Params *, EmulNet *, Log *, Address *);
